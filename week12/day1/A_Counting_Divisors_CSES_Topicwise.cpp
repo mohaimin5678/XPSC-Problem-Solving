42f3548b74dbdec9e288af0d131849d12f6ad22c
mohaimin5678 <91788048+mohaimin5678@users.noreply.github.com>
2025-01-15 04:22:52 +0600

    Update A_Counting_Divisors_CSES_Topicwise.cpp

--- week12/day1/A_Counting_Divisors_CSES_Topicwise.cpp
+++ week12/day1/A_Counting_Divisors_CSES_Topicwise.cpp
@@ -2,10 +2,55 @@
 #define ll long long int
 #define endl '\n'
 using namespace std;
+const int maxN=1e6+9;
+vector<int> divisors(maxN);
+// vector<int> divisors[maxN];  //to find all divisor numbers
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    
+    for(int i=1;i<maxN;i++){
+        for(int j=i;j<maxN;j+=i){
+            divisors[j]++;
+        }
+    }
+
+    //to find all divisor numbers
+    // for(int i=1;i<maxN;i++){
+    //     for(int j=i;j<maxN;j+=i){
+    //         divisors[j].push_back(i);
+    //     }
+    // }
+
+    //see the counts
+    // for(int i=1;i<=30;i++){
+    //     cout<<i<<" ->  "<<divisors[i]<<endl;
+    // }
+
+    //to find all divisor numbers
+    // for(int i=1;i<=30;i++){
+    //     cout<<i<<" ->  ";
+    //     for(int x:divisors[i]){
+    //         cout<<x<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<divisors[n]<<endl;
+    }
     return 0;
 }
+/*
+Sieve algorithm way te amra multiple gula te traverse
+korbo & jekhane jekhane multiple pabo shekhane shekhane
+either pushback/++ kore "numbers save"/"numbers count"
+kore rekhe dibo. complexity O(Nlog(log(N)))
+then input niye O(1) e sheta execution
+kore dibo. 
+
+*/
