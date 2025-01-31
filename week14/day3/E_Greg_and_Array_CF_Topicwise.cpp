23f611a01c02ee837cdceb5b011665a30f9a1c5d
mohaimin5678 <91788048+mohaimin5678@users.noreply.github.com>
2025-02-01 03:15:42 +0600

    Update E_Greg_and_Array_CF_Topicwise.cpp

--- week14/day3/E_Greg_and_Array_CF_Topicwise.cpp
+++ week14/day3/E_Greg_and_Array_CF_Topicwise.cpp
@@ -45,19 +45,19 @@ void mohaimin(){
         (store[i].second)*=s[i];
     }
 
-    vector<ll> ans(n+2);
+    vector<ll> op(n+2);
     for(int i=1;i<=m;i++){
         // v[i]=v[i-1]+v[i];
         // cout<<v[i]<<" ";
-        ans[store[i].first.first]+=store[i].second;
-        ans[store[i].first.second+1]-=store[i].second;
+        op[store[i].first.first]+=store[i].second;
+        op[store[i].first.second+1]-=store[i].second;
     }
-    for(int i=1;i<ans.size();i++){
-        ans[i]=ans[i-1]+ans[i];
+    for(int i=1;i<op.size();i++){
+        op[i]=op[i-1]+op[i];
     }
 
     for(int i=1;i<=n;i++){
-        v[i]+=ans[i];
+        v[i]+=op[i];
     }
     for(int i=1;i<=n;i++){
         cout<<v[i]<<" ";
@@ -73,12 +73,69 @@ int main(){
     
 }
 /*
+Here its a different type of "difference array" 
+first they want us to perform M operations & then we 
+need to do those operation K times where they'll give us
+operation range!
+
+Observation: We don't need to do M operation initially!
+Lets store it first & then we'll check how many times
+these operations we'll need to do. (On K queries)
+
+Explanation: 
+i) we initialize the vector & keep that as it is. 
+ii) We made a pair<pair<int,int>,ll> vector to store the operations. 
+    We did it with indexing because it will be needed. 
+iii) We took another vector S to calculate the operations which 
+    is given on K queries. We did the difference array concept 
+    on this. After doing s[x]++ & s[y+1]--. We took the prefix 
+    sum & by that now we can understand how many times we'll do 
+    each operation. 
+iv) Now that we know how many times we'll perform a particular 
+    operation, we can simply do (d × times), which will make d 
+    the same as if we had performed the operation that many times 
+    separately.
+v) Now we took a vector op to perform the operations using
+   the difference array concept & after doing the operations we
+   we'll calculate the prefix sum. 
+vi) finally we'll add them with the initial vector v & that will
+    the final answer.
+
+testcase: 
+3 3 3
+1 2 3
+1 2 1
+1 3 2
+2 3 4
+1 2
+1 3
+2 3
+
+v=[1 2 3]
+
+store=[{{1 2},1}, {{1 3},2}, {{2 3},4}]
+now K queries:- 
 0 1  2  3  4
   1    -1
   1       -1
      1    -1
 --------------
-0 2  3  2 0        prefix sum
+0 2  3  2 0    -->prefix sum
+operations contribution which is s. so s=[2 3 2]
 
-operations contribution
+now lets change d's value on store vector using s. 
+(store[i].second)*=s[i];
+now store=[{{1 2},2}, {{1 3},6}, {{2 3},8}]
+
+lets take op vector to do the operations: 
+0 1  2  3  4
+  2    -2
+  6       -6
+     8    -8
+------------
+0 8 16 14  0
+so ans=[8 16 14]
+     v=[1  2  3]
+       ---------
+     v=[9 18 17]    final answer
 */
