b05fd5bc5ffc19674e03de5de6352ad3cb7a3ee4
mohaimin5678 <91788048+mohaimin5678@users.noreply.github.com>
2025-02-01 22:40:48 +0600

    Add files via upload
    codes of week 14 day 4

---
+++ week14/day4/D_Appending_Mex_CF.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ln '\n'
+#define cyes cout<<"YES"<<ln
+#define cno cout<<"NO"<<ln
+#define yes cout<<"Yes"<<ln
+#define no cout<<"No"<<ln
+using namespace std;
+void mohaimin(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    int serial=-1;
+    for(int i=0;i<n;i++){
+        if(v[i]>serial+1){
+            cout<<i+1<<ln;
+            return;
+        }
+        serial=max(serial,v[i]);
+    }
+    cout<<-1<<ln;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    t=1;
+    while(t--){
+        mohaimin();
+    }
+}
