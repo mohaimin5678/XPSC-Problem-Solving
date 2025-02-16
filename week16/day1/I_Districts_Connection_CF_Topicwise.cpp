98d6043e88fc71d05a6993cca70dc4cd83fd7d8f
mohaimin5678 <91788048+mohaimin5678@users.noreply.github.com>
2025-02-17 04:34:11 +0600

    Update I_Districts_Connection_CF_Topicwise.cpp

--- week16/day1/I_Districts_Connection_CF_Topicwise.cpp
+++ week16/day1/I_Districts_Connection_CF_Topicwise.cpp
@@ -7,7 +7,33 @@
 #define no cout<<"No"<<ln
 using namespace std;
 void mohaimin(){
-    
+    int n;
+    cin>>n;
+    vector<int> v(n+1);
+    for(int i=1;i<=n;i++) cin>>v[i];
+    bool flag=false;
+    for(int i=1;i<=n;i++){
+        if(v[1]!=v[i]){
+            flag=true;
+            break;
+        }
+    }
+    if(flag){
+        cyes;
+        int city=-1;
+        for(int i=2;i<=n;i++){
+            if(v[1]!=v[i]){
+                cout<<1<<" "<<i<<ln;
+                city=i;
+            }
+        }
+        for(int i=2;i<=n;i++){
+            if(v[1]==v[i]){
+                cout<<city<<" "<<i<<ln;
+            }
+        }
+    }
+    else cno;
 }
 int main(){
     ios::sync_with_stdio(false);
@@ -19,3 +45,39 @@ int main(){
         mohaimin();
     }
 }
+/*
+jekono 1ta gang er 1ta district ke center e rekhe baki shob different 
+gang er districts gulo ke oi center district er shathe connect kore
+dibo. Then oi selected gang er ar remaining districts thakle center 
+er shathe connected jekono district er shathe connect kore dibo. 
+testcase: 
+5
+1 2 2 1 3       
+
+now indices are districts
+lets select 1 as center then
+          2
+          |
+    2----[1]----3
+since there is one more 1st gang district remaining so lets connect
+it with 3rd gang.
+          2
+          |
+    2----[1]----[3]
+                 |
+                 1
+this is gang representation. 
+now v=[1 2 2 1 3]   gangs
+index=[1 2 3 4 5]   districts
+          3
+          |
+    2----[1]----[5]
+                 |
+                 4
+so ans: 
+YES
+1 2
+1 3
+1 5
+5 4
+*/
