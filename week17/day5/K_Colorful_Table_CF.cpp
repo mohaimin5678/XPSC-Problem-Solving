4363be9a2eddab6acf440568f11078699e6f5db5
mohaimin5678 <91788048+mohaimin5678@users.noreply.github.com>
2025-02-28 23:47:45 +0600

    Update K_Colorful_Table_CF.cpp without description

--- week17/day5/K_Colorful_Table_CF.cpp
+++ week17/day5/K_Colorful_Table_CF.cpp
@@ -7,7 +7,31 @@
 #define no cout<<"No"<<ln
 using namespace std;
 void mohaimin(){
-    
+    int n,k;
+    cin>>n>>k;
+
+    vector<int> v(n);
+    for(int i=0;i<n;i++) cin>>v[i];
+
+    vector<vector<int>> pos(k);
+    set<int> st;
+
+    for(int i=0;i<n;i++){
+        pos[v[i]-1].push_back(i);   //0-base indexing
+        st.insert(i);
+    }
+
+    vector<int> ans(k);
+    for(int x=0;x<k;x++){
+        if(!pos[x].size()) continue;
+
+        ans[x]= 2*(*st.rbegin()-*st.begin()+1);
+        for(auto p:pos[x]){
+            st.erase(p);
+        }
+    }
+    for(int x:ans) cout<<x<<" ";
+    cout<<ln;
 }
 int main(){
     ios::sync_with_stdio(false);
@@ -19,3 +43,6 @@ int main(){
         mohaimin();
     }
 }
+/*
+
+*/
