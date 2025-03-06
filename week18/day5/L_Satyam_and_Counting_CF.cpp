4abdf101c76015ccca236b909ed3e6622dd7edf2
mohaimin5678 <91788048+mohaimin5678@users.noreply.github.com>
2025-03-07 03:31:17 +0600

    Update L_Satyam_and_Counting_CF.cpp

--- week18/day5/L_Satyam_and_Counting_CF.cpp
+++ week18/day5/L_Satyam_and_Counting_CF.cpp
@@ -66,3 +66,32 @@ int main(){
         mohaimin();
     }
 }
+/*
+since we need to make triangle with 90∘ on it, 
+we can check if we get {x,0} & {x,1} on two pair vectors. when we find
+then we'll just calculate count. And we'll break the loop if any of l/r
+reach their corresponding size limit. 
+
+now first we'll calculate for lower side(0's side), so we'll check how 
+many triangle we can make on left(means current l's value since its 0-based)
+& how many triangle we can make on right(means size-l-1 since its 0-based)
+then add both of them to count. 
+same for upper side(1's side) just the change is now we'll calculate with
+r. then add both of them to count too. 
+
+now for 2 more type of triangle shape remains to calculate!
+they are:       __      &       
+                \/              /_\
+
+for first type of triangle: 
+we'll take a value from A set(0's side) & check value-1 & value+1 both
+are exists on B set(1's side). if they are exists then we can make first
+type of triangle with this 3 position. so count++;
+
+for second type of triangle: 
+we'll take a value from B set(1's side) & check value-1 & value+1 both
+are exists on A set(0's side). if they are exists then we can make second
+type of triangle with this 3 position. so count++;
+
+after counting all possible triangle counting, we'll just print the count. 
+*/
