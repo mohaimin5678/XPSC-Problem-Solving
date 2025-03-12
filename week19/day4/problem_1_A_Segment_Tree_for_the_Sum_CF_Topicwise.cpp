1badf8cc3e977806a1e5296ab59e947273d6b7e6
mohaimin5678 <91788048+mohaimin5678@users.noreply.github.com>
2025-03-12 23:52:40 +0600

    Add files via upload
    codes of week 19 day 4

---
+++ week19/day4/problem_1_A_Segment_Tree_for_the_Sum_CF_Topicwise.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ln '\n'
+#define cyes cout<<"YES"<<ln
+#define cno cout<<"NO"<<ln
+#define yes cout<<"Yes"<<ln
+#define no cout<<"No"<<ln
+using namespace std;
+const int maxN=1e5+9;
+ll a[maxN], t[4*maxN];      //t is for tree
+void build(int n,int b,int e){          //O(N)
+    if(b==e){
+        t[n]=a[b];      //a[e] o dite parbo since index same e. oi index
+        return;         //er value tai node e save hocche
+    }
+    int mid=(b+e)/2;
+    int l=2*n, r=(2*n)+1;
+    build(l,b,mid);
+    build(r,mid+1,e);
+    t[n]=t[l]+t[r];     
+    //backtrack kore ashar por tar 2 child er sum e hobe n node er sum
+}
+
+void update(int n,int b,int e,int i,int v){     //O(logN)
+    if(i<b || i>e){   //i range er baire so niche ar jawar dorkar nai
+        return;     //"out of" segment
+    }
+    if(b==e){
+        t[n]=v;  //particular index e chole ashchi so node update korbo
+        return;  //"inside" segment
+    }
+    //"intersect" segment
+    int mid=(b+e)/2;
+    int l=2*n, r=(2*n)+1;
+    update(l,b,mid,i,v);
+    update(r,mid+1,e,i,v);
+    t[n]=t[l]+t[r];
+    //backtrack kore ashar por tar 2 child er sum e hobe n node er sum
+}
+ll query(int n,int b,int e,int i,int j){     //O(logN)
+    if(e<i || b>j){   //"out of" segment
+        return 0;     //particular segment query range er baire
+    }
+    if(b>=i && e<=j){   //"inside" segment
+        return t[n];    //particular segment query range er fully vitore
+                        //tai oi segment er sum tai return diye dicchi
+    }
+    //"intersect" segment
+    int mid=(b+e)/2;
+    int l=2*n, r=(2*n)+1;
+    return query(l,b,mid,i,j) + query(r,mid+1,e,i,j); 
+    //just both left+right child segment theke asha sum add kore return
+    //kore dicchi
+}
+void mohaimin(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    build(1,1,n);
+    while(m--){
+        int t;
+        cin>>t;
+        if(t==1){
+            int idx,v;
+            cin>>idx>>v;
+            idx++;
+            update(1,1,n,idx,v);
+        }
+        else{
+            int l,r;
+            cin>>l>>r;
+            l++;
+            ll ans=query(1,1,n,l,r);
+            cout<<ans<<ln;
+        }
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    t=1;
+    while(t--){
+        mohaimin();
+    }
+}
