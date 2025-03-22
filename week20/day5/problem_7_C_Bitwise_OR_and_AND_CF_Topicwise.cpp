956fd964f9123cee9724befcfe9072acffa50bde
mohaimin5678 <91788048+mohaimin5678@users.noreply.github.com>
2025-03-22 19:27:52 +0600

    Add files via upload
    codes of week 20 day 5

---
+++ week20/day5/problem_7_C_Bitwise_OR_and_AND_CF_Topicwise.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ln '\n'
+#define cyes cout<<"YES"<<ln
+#define cno cout<<"NO"<<ln
+#define yes cout<<"Yes"<<ln
+#define no cout<<"No"<<ln
+using namespace std;
+const int maxN=1e5+9;
+ll a[maxN], t[4*maxN], lazy[4*maxN];      //t is for tree
+
+void push(int n,int b,int e){
+    if(lazy[n]==0) return;      //no lazy value so return
+    t[n]= (t[n] | lazy[n]);  
+    // cout<<t[n]<<ln;  
+    if(b!=e){       //not a leaf node then
+        int l=2*n, r=(2*n)+1;
+        lazy[l]|=lazy[n];   
+        lazy[r]|=lazy[n];
+    }
+    lazy[n]=0; //lazy part er kaj shesh howar por lazy reset to -1.
+    //cause node can be updated by 0 also. so returing when finding 
+    //0 caused WA for a long time. returning -1 solved the issue. 
+}
+
+void build(int n,int b,int e){          //O(N)
+    if(b==e){
+        t[n]=a[b];      //a[e] o dite parbo since index same e. oi index
+        return;         //er value tai node e save hocche
+    }
+    int mid=(b+e)/2;
+    int l=2*n, r=(2*n)+1;
+    build(l,b,mid);
+    build(r,mid+1,e);
+    t[n]=(t[l] & t[r]);
+}
+
+void update(int n,int b,int e,int i,int j,ll v){     //O(logN)
+    push(n,b,e); //visit er shathe shathe lazy ta update kore dibo
+    if(j<b || i>e){ //i,j range er baire so niche ar jawar dorkar nai
+        return;     //"out of" segment
+    }
+    if(b>=i && e<=j){
+        //particular ei node er under e all node V diye update hobe
+        lazy[n]|=v; 
+        push(n,b,e); //visit er shathe shathe lazy ta update kore dibo
+        return;  //"inside" segment
+    }
+    //"intersect" segment
+    int mid=(b+e)/2;
+    int l=2*n, r=(2*n)+1;
+    update(l,b,mid,i,j,v);
+    update(r,mid+1,e,i,j,v);
+    t[n]=(t[l] & t[r]);
+}
+ll query(int n,int b,int e,int i,int j){     //O(logN)
+    push(n,b,e); //visit er shathe shathe lazy ta update kore dibo
+    if(e<i || b>j){   //"out of" segment
+        return (1LL<<31)-1;     //particular segment query range er baire
+    }
+    if(b>=i && e<=j){   //"inside" segment
+        return t[n];    //particular segment query range er fully vitore
+                        //tai oi segment er sum tai return diye dicchi
+    }
+    //"intersect" segment
+    int mid=(b+e)/2;
+    int l=2*n, r=(2*n)+1;
+    return (query(l,b,mid,i,j) & query(r,mid+1,e,i,j));  
+}
+void mohaimin(){
+    int n,m;
+    cin>>n>>m;
+    build(1,1,n);
+    while(m--){
+        int t;
+        cin>>t;
+        if(t==1){
+            int l,r;
+            ll v;
+            cin>>l>>r>>v;
+            l++;
+            update(1,1,n,l,r,v);
+        }
+        else{
+            int l,r;
+            cin>>l>>r;
+            l++;
+            ll ans=query(1,1,n,l,r);
+            cout<<ans<<ln;
+        }
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    t=1;
+    while(t--){
+        mohaimin();
+    }
+}
